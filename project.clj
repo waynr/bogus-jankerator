569177f4122b21b19652dba74736ea6eaeb4f1b3
Jenkins CI <ci@puppetlabs.com>
2015-03-06 08:31:38 -0800

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/bogus-jankerator "0.1.1-SNAPSHOT"
+(defproject puppetlabs/bogus-jankerator "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
