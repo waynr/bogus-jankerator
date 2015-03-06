d28d42f0a22b3eb4715ce5a2d0f6f54e45b25831
Wayne <wayne@puppetlabs.com>
2015-03-06 08:31:31 -0800

    Add deploy-info

--- project.clj
+++ project.clj
@@ -1,6 +1,17 @@
+(
+(defn deploy-info
+  [url]
+  { :url url
+    :username :env/nexus_jenkins_username
+    :password :env/nexus_jenkins_password
+    :sign-releases false })
+
 (defproject puppetlabs/bogus-jankerator "0.1.2-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  :deploy-repositories [["releases" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/releases/")]
+                        ["snapshots" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/")]])
